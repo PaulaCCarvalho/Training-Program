b5e21e34aa8dc0ed73f438685494d2523432f066
Paula Cristina Carvalho Vilela <paulinhasgst@gmail.com>
2022-09-18 14:14:20 -0300

    Estrurando o  projeto

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import './styles/main.css'
+
+function App(){
+    return (
+        <div className="text-white">
+            <h1>Hello World!</h1>
+        </div>
+    )
+}
+
+export default App;
