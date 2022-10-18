d6f688a0e687aea9e2c960bb94480f1a29ca5d77
Aladjah <aladjaheecol@gmail.com>
2022-10-18 00:17:40 -0300

    Please enter the commit message for your changes. Lines starting

--- Backend/src/Router/member.js
+++ Backend/src/Router/member.js
@@ -1,6 +1,7 @@
 const {Router} = require('express');
-
+const {MemberControler} = require('../Controller');
 const memberRouter = Router();
+const MemberControler = new MemberControler();
 memberRouter.get('/member', (req, res) => {
     res.send('<h1>Member</h1>');
 })
